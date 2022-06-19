349b2e6ddb870e6228474871effc45390c911d1b
alwayscompiling <ian.rettig13@gmail.com>
2022-06-19 17:04:31 -0400

    Adding deploy commands.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
 // Login to Discord with your client's token
