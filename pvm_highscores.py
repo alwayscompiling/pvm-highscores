692d43f8ae8b3073747eeaa71cba0e78f7136612
alwayscompiling <ian.rettig13@gmail.com>
2022-06-22 21:30:01 -0400

    Adding logging, ignoring log file, cleaning up python file lint warnings.

--- pvm_highscores.py
+++ pvm_highscores.py
@@ -1,19 +1,35 @@
+"""
+pvm_highscores discord bot
+"""
+
 import json
+import logging
 import discord
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 
 client = discord.Client()
 
-with open("config.json") as file:
+with open("config.json", encoding='utf-8', mode='r') as file:
     config_data = json.load(file)
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    """"reports when bot logs in"""
+    print(f'We have logged in as {client.user}')
 
 
 @client.event
 async def on_message(message):
+    """"runs when message is sent on server"""
     print("message get")
     if message.author == client.user:
         return
