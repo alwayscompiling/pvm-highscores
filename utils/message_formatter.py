17cb66870a8df27dd18e2cf67c8a9d7f55ddc50a
alwayscompiling <ian.rettig13@gmail.com>
2022-06-26 17:10:39 -0400

    Create format boss highscore function.

---
+++ utils/message_formatter.py
@@ -0,0 +1,42 @@
+"""
+Contains various function to format the highscore messages
+"""
+
+import highscores  # pylint: disable=import-error
+
+
+def format_boss_highscore(boss):
+    """
+    returns a formattedd string for a boss's highscore
+    @param the dictionary object for the boss
+    @return the formatted string
+    """
+    boss_name = boss["boss"]
+    ret_string = f"**{boss_name}**/n"
+
+    # Gather all categories and add to string
+    for category in highscores.highscores_config["categories"]:
+        category_name = category["category"]
+        ret_string += f"{category_name}\t\t"
+
+    highscore_size = highscores.highscores_config["highscore_size"]
+
+    # list rankings
+    for x in range(highscore_size):
+        for category in highscores.highscores_config["categories"]:
+            ret_string += f"{x}: (name here)\t\t"
+        ret_string += "\n"
+
+    # if boss is hardmode, repeat above for hardmode applicable categories
+    if boss["hardmode"]:
+        for category in highscores.highscores_config["categories"]:
+            if category["hardmode"]:
+                category_name = category["category"]
+                ret_string += f"{category_name}\t\t"
+
+        ret_string += "\n"
+
+        for x in range(highscore_size):
+            for category in highscores.highscores_config["categories"]:
+                ret_string += f"{x}: (name here)\t\t"
+            ret_string += "\n"
