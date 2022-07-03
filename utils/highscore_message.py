b1b22fb595cfc7a3d4efa0a9d4ee77f734ef3d26
alwayscompiling <ian.rettig13@gmail.com>
2022-07-03 10:01:56 -0400

    Changing message to be a code block for monospace. Easier formatting.

--- utils/highscore_message.py
+++ utils/highscore_message.py
@@ -13,12 +13,12 @@ def format_highscore_message(boss_name: str):
     @param boss: the dictionary object for the boss
     @return the formatted string
     """
-    ret_string = f"**{boss_name}**\n"
+    ret_string = f"**{boss_name}**\n```"
     boss_data = highscores_data[boss_name]
 
     # Gather all categories and add to string
     for key, value in boss_data["categories"].items():
-        ret_string += f"{key}".ljust(50, " ")
+        ret_string += f"{key}".ljust(30, " ")
     ret_string += "\n"
 
     highscore_size = highscores_config["highscore_size"]
@@ -28,17 +28,18 @@ def format_highscore_message(boss_name: str):
         for key, value in boss_data["categories"].items():
             if len(value) > i:
                 score = value[i]
-                ret_string += f"{i+1}: {score[0]} - {score[1]}".ljust(50, " ")
+                ret_string += f"{i+1}: {score[0]} - {score[1]}".ljust(30, " ")
             else:
-                ret_string += f"{i+1}: submit your score".ljust(50, " ")
+                ret_string += f"{i+1}: submit your score".ljust(30, " ")
         ret_string += "\n"
 
-    return ret_string
+    return ret_string + "```"
 
 
 async def send_highscore_message(channel, boss_name: str):
     """
-    Function takes care of checking if a message exists for a boss, then creating and sending/editing the message
+    Function takes care of checking if a message exists for a boss, then
+    creating and sending/editing the message
     @param channel: the channel to send the message in/search for message id
     @param boss: the boss to send the message about
     """
